b7ee3926f017f6dd2c8e72e62acebfebb2f941e8
myildiz17 <muhammetyildiz@gmail.com>
2021-02-04 19:39:47 -0600

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+
+
+
+export default class EyesOnMe extends Component {
+
+    render(){
+
+        return(
+            <div>
+                <button onFocus={()=>{console.log("onfocus")}} onBlur={()=>{console.log("on blur")}}>Button</button>
+            </div>
+        )
+    }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+
+export default class Keypad extends Component {
+
+    handleKeyUp = ()=>{
+        console.log("Entering Password...")
+    }
+    render(){
+
+        return(
+            <div>
+                <input onKeyUp={this.handleKeyUp} type="text"></input>
+            </div>
+        )
+    }
+
+
+}
